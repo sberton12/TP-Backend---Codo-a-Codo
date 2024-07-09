701f42420956159865437939e14ded8e4f62650c
Sergio Nicolas Berton <sergionberton@gmail.com>
2024-07-09 17:02:39 -0300

    error en comments

--- controller/commentsController.js
+++ controller/commentsController.js
@@ -2,7 +2,7 @@ const db = require('../db/db');
 
 const getComments = (req, res) => {
     const sql = 'SELECT * FROM mydb23.commentstable';
-    db.query(sql,[id], (err, result) => {
+    db.query(sql, (err, result) => {
         if (err) throw err;
         res.send(result); 
     });
