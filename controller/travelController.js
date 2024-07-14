43b719dfd26e202980a30ba94a86cb5a33a61318
Sergio Nicolas Berton <sergionberton@gmail.com>
2024-07-13 22:50:11 -0300

    UPDATE

--- controller/travelController.js
+++ controller/travelController.js
@@ -2,7 +2,7 @@ const db = require('../db/db');
 
 const getTravels = (req, res) => {
     const sql = 'SELECT * FROM mydb23.travelstable';
-    db.query(sql,[id], (err, result) => {
+    db.query(sql, (err, result) => {
         if (err) throw err;
         res.send(result); 
     });
